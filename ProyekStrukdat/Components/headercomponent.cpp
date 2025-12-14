f2c1409e3bddfa7f9b087228a8bde9631ddf161d
unknown <donisimamora15@gmail.com>
2025-12-14 16:10:12 +0700

    Header Fixed As Componnets

--- ProyekStrukdat/Components/headercomponent.cpp
+++ ProyekStrukdat/Components/headercomponent.cpp
@@ -5,21 +5,23 @@
 #include <QPainter>
 #include <QPainterPath>
 
+using namespace Qt;
+
 HeaderComponent::HeaderComponent(QWidget *parent) : QWidget(parent){
     QVBoxLayout *layout = new QVBoxLayout(this);
     layout->setContentsMargins(0, 0, 0, 0);
     layout->setSpacing(0);
 
     QLabel *imageLabel = new QLabel(this);
-    imageLabel->setAlignment(Qt::AlignCenter);
+    imageLabel->setAlignment(AlignCenter);
 
     QPixmap pix(":/logo/logo/lokasi.png");
 
     int size = 50;
-    QPixmap scaledPix = pix.scaled(size, size, Qt::KeepAspectRatio, Qt::SmoothTransformation);
+    QPixmap scaledPix = pix.scaled(size, size, KeepAspectRatio, SmoothTransformation);
 
     QPixmap roundedPix(scaledPix.size());
-    roundedPix.fill(Qt::transparent);
+    roundedPix.fill(transparent);
 
     QPainter painter(&roundedPix);
     painter.setRenderHint(QPainter::Antialiasing);
